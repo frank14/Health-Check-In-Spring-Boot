f5b86ef173304e50260c52cbd55a23b683c6be75
frank14 <frank_vega25@hotmail.com>
2020-02-02 22:16:07 -0500

    upload healt file

---
+++ Heatl-Check/src/main/java/com/app/api/controller/EmployeeVO.java
@@ -0,0 +1,15 @@
+package com.app.api.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeVO {
+
+	private String first_name;
+	private String last_name;
+
+}
