1b39a2662d86e588f7890c1d6c69b7b32397f3fb
frank14 <frank_vega25@hotmail.com>
2020-02-03 21:46:58 -0500

    delete imports

--- Heatl-Check/src/main/java/com/app/api/HeatlCheckApplication.java
+++ Heatl-Check/src/main/java/com/app/api/HeatlCheckApplication.java
@@ -2,16 +2,9 @@ package com.app.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.google.common.base.Predicates;
-
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
