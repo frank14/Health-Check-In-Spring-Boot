91f5622d3936b9d889893bf302b14f075518c071
frank14 <frank_vega25@hotmail.com>
2020-02-07 20:52:45 -0500

    upate

---
+++ Heatl-Check/src/main/java/com/app/api/controller/ApplicationController.java
@@ -0,0 +1,28 @@
+package com.app.api.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class ApplicationController {
+
+	@Bean
+	public Docket api() {
+	return new Docket(DocumentationType.SWAGGER_2)
+	    .select()
+	    .apis(RequestHandlerSelectors.any())
+	    .paths(PathSelectors.any())
+	    .paths(Predicates.not(PathSelectors.regex("/error.*")))
+	    .paths(Predicates.not(PathSelectors.regex("/admin.*")))
+	    .paths(Predicates.not(PathSelectors.regex("/actuator.*")))
+	    .build();
+	}
+
+}
