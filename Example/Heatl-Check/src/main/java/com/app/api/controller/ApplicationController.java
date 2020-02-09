1307701312a9632c29e9a2475f25ed2fa305f154
frank14 <frank_vega25@hotmail.com>
2020-02-09 11:29:06 -0500

    separate files of configuration

--- Example/Heatl-Check/src/main/java/com/app/api/controller/ApplicationController.java
+++ Example/Heatl-Check/src/main/java/com/app/api/controller/ApplicationController.java
@@ -15,22 +15,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@Configuration
 @Controller
 public class ApplicationController {
-
-	@Bean
-	public Docket api() {
-	return new Docket(DocumentationType.SWAGGER_2)
-	    .select()
-	    .apis(RequestHandlerSelectors.any())
-	    .paths(PathSelectors.any())
-	    .paths(Predicates.not(PathSelectors.regex("/error.*")))
-	    .paths(Predicates.not(PathSelectors.regex("/admin.*")))
-	    .paths(Predicates.not(PathSelectors.regex("/actuator.*")))
-	    .paths(Predicates.not(PathSelectors.regex("/")))
-	    .build();
-	}
 	
 	@Value("${uri}")
 	private String hostUrl;
